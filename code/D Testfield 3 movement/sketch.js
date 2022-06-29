ee9c11ba97703b2ffa570fb968038ec36886a5d1
Dennis <herzogden92@gmail.com>
2022-06-29 21:36:43 +0200

    finales Update

--- code/D Testfield 3 movement/sketch.js
+++ code/D Testfield 3 movement/sketch.js
@@ -6,13 +6,19 @@ const sketchWidth = 400;
 const sketchHeight = 400;
 const size = 20;
 
+let scalefaktor = 1;
 let points = [];
 
 function setup() {
+  frameRate(30);
+  createLoop({
+    duration: 3, // duration in seconds for our gif animation
+    gif: true,
+  });
   createCanvas(sketchWidth, sketchHeight);
   fill(255);
   strokeWeight(2);
-
+  background(0);
   for (let i = 0; i < 100; i += 1) {
     points.push({
       radius: random(30, 70),
@@ -22,14 +28,14 @@ function setup() {
 }
 
 function draw() {
-  background(255, 7);
+  background(0, 7);
   stroke(255, 150, 100);
-  //rect(0, 0, sketchWidth, sketchHeight);
 
   translate(sketchWidth / 2, sketchHeight / 2);
-
+  scale(scalefaktor);
+  scalefaktor += 0.0005;
   for (let i = 0; i < points.length; i += 1) {
-    points[i].angle += 1.5;
+    points[i].angle += 3;
 
     const rad = (Math.PI / 180) * points[i].angle;
     let radius = points[i].radius;
@@ -38,22 +44,20 @@ function draw() {
     const y = radius * Math.sin(rad);
 
     ellipse(x, y, 2, 2);
-  }
-
-  for (let a = 0; a < points.length; a += 1) {
-    points[a].angle += 1;
-
-    const rad = (Math.PI / 180) * points[a].angle;
-    let radius = points[a].radius;
-    const x = radius * Math.cos(rad);
-    const y = radius * Math.sin(rad);
-
     ellipse(x * 2, y / 3, 2, 2);
   }
-  noStroke();
-  arc(0, -5, 56, 50, (Math.PI / 180) * -180, (Math.PI / 180) * 0, OPEN);
-}
 
-// hintergrund schwarz machen
-// perfekter: eine Bewegung in der gesamten Form
-// Form expandiert leicht
+  //for (let a = 0; a < points.length; a += 1) {
+  //  points[a].angle += 1;
+  //
+  //  const rad = (Math.PI / 180) * points[a].angle;
+  //  let radius = points[a].radius;
+  //  const x = radius * Math.cos(rad);
+  //  const y = radius * Math.sin(rad);
+  //
+  //  ellipse(x * 2, y / 3, 2, 2);
+  //}
+  fill(0);
+  noStroke();
+  arc(0, -3, 56, 50, (Math.PI / 180) * -180, (Math.PI / 180) * 0, OPEN);
+}
--- code/D Testfield 4 gif erstellen/sketch.js
+++ code/D Testfield 4 gif erstellen/sketch.js
@@ -8,11 +8,10 @@ const size = 20;
 
 let scalefaktor = 1;
 let points = [];
-let verlaufB = 255;
 
 function setup() {
   createLoop({
-    duration: 5, // duration in seconds for our gif animation
+    duration: 15, // Gif-Sekunden
     gif: true,
   });
   createCanvas(sketchWidth, sketchHeight);
@@ -23,44 +22,63 @@ function setup() {
     points.push({
       radius: random(30, 70),
       angle: random(0, 360),
-      //verlaufB: 255 - i * 0.5,
     });
   }
 }
 
 function draw() {
   background(0, 7);
-
   translate(sketchWidth / 2, sketchHeight / 2);
   scale(scalefaktor);
 
-  scalefaktor += 0.001;
+  scalefaktor += 0.001; //Faktor wie schnell sich die Gesamtfläche vergrößert
   for (let i = 0; i < points.length; i += 1) {
-    // stroke(255, 235, points[i].verlaufB);
     noStroke();
-    points[i].angle += 4;
+    points[i].angle += 4; //speed
 
     const rad = (Math.PI / 180) * points[i].angle;
     let radius = points[i].radius;
 
-    const x = radius * Math.cos(rad);
+    const x = radius * Math.cos(rad); //kreisform
     const y = radius * Math.sin(rad);
 
-    //stroke(255, 235, 220);
-    fill(255, 235, 220);
+    fill(255, 235, 220, 255 - (255 / 40) * (points[i].radius - 30)); //farbe geht von weiß nach außen hin zu transparent
     ellipse(x, y, 4, 4);
+
     fill(
       lerpColor(
+        //farbtausch zwischen weiß und leichtem orange
         color(255, 255, 255),
-        color(255, 200, 150),
+        color(255, 200, 150, 0),
         (points[i].radius - 30) / 40
       )
     );
-
     ellipse(x * 2, y / 3, 4, 4);
   }
 
   fill(0);
-  noStroke();
+
   arc(0, -3, 56, 50, (Math.PI / 180) * -180, (Math.PI / 180) * 0, OPEN);
+
+  const alphaOffset = 300;
+
+  fill(0, rectAlpha - alphaOffset);
+  if (direction == true) {
+    rectAlpha += 1;
+  } else {
+    rectAlpha -= 1;
+  }
+
+  if (rectAlpha > 255 + alphaOffset) {
+    direction = false;
+    scalefaktor = 1;
+  }
+  if (rectAlpha < 0) {
+    direction = true;
+  }
+
+  rect(-200, -200, 400, 400);
 }
+
+let rectAlpha = 255;
+let direction = false;
